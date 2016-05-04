8c5a4388e84ef150666a0766da89cc9dc321054f
afc163 <afc163@gmail.com>
2016-05-04 13:51:23 +0800

    fix log

--- src/index.js
+++ src/index.js
@@ -17,15 +17,15 @@ module.exports = {
       while (part = yield parts) {
         if (part.length) {
           // arrays are busboy fields
-          log.info('dora-plugin-upload', `'key: ${part[0]}`);
-          log.info('dora-plugin-upload', `'value: ${part[1]}`);
+          log.info('dora-plugin-upload', `key: ${part[0]}`);
+          log.info('dora-plugin-upload', `value: ${part[1]}`);
         } else {
           // otherwise, it's a stream
           log.info('dora-plugin-upload', `mocking file upload pipe`);
           part.pipe(devnull());
         }
       }
-      log.info('dora-plugin-upload', `'done parsing the upload form!`);
+      log.info('dora-plugin-upload', `done parsing the upload form!`);
       this.status = 200;
       this.body = {
         'status': 'success',
